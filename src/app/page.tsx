80af52deb65d7a60d93a0f22d00e747e840be677
Sergey <segy@exdesis.com>
2024-05-04 11:35:44 +0200

    Change next button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -129,7 +129,7 @@ const Home = (): React.ReactElement => {
           </div>
           <button
             type="submit"
-            className="bg-indigo-500 text-white px-4 py-2  rounded-md btn"
+            className="bg-indigo-500 text-white px-4 py-2  rounded-md max-h-[40px] self-end align-self-end"
             disabled={!selectedOrigin}
           >
             Next
